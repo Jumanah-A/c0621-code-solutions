3e7711fc290987de68c3e81e47b249c5bcf37278
Jumanah Almajnouni <jumanahh@outlook.com>
2021-06-28 10:55:13 -0700

    Comp[leted javascript-priomitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+var fullName = 'Jumanah Almajnouni';
+var isCool = true;
+var totalPets = 0;
+var unknown = null;
+var never;
+
+console.log('value of fullName: ', fullName);
+console.log('value of isCool: ', isCool);
+console.log('value of totalPets: ', totalPets);
+
+console.log('typeof fullName: ', typeof 'Jumanah Almajnouni');
+console.log('typeof isCool: ', typeof true);
+console.log('typeof totalPets: ', typeof 0);
+
+console.log('value of unknown: ', unknown);
+console.log('typeof unknown: ', typeof null);
+console.log('value of never: ', never);
+console.log('typeof never: ', typeof never);
