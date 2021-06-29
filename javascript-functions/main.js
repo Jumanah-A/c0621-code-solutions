b2d3251a1bbde07c2499fd4ef36f5bf220a09794
Jumanah Almajnouni <jumanahh@outlook.com>
2021-06-29 15:41:58 -0700

    Fixed typo

--- javascript-functions/main.js
+++ javascript-functions/main.js
@@ -10,7 +10,7 @@ function getArea(width, height) {
   return width * height;
 }
 
-function getFisrtName(person) {
+function getFirstName(person) {
   return person.firstName;
 }
 
@@ -20,7 +20,7 @@ function getLastElement(array) {
 var minutesToSeconds = convertMinutesToSeconds(5);
 var greeting = greet('Jumanah');
 var area = getArea(17, 42);
-var firstName = getFisrtName({ firstName: 'Jumanah', lastName: 'Almajnouni' });
+var firstName = getFirstName({ firstName: 'Jumanah', lastName: 'Almajnouni' });
 var lastElement = getLastElement(['apple', 'banana', 'orange', 'berries']);
 console.log('The value of minutesToSeconds is: ', minutesToSeconds);
 console.log('The value of greeting is: ', greeting);
