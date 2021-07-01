28534795ddbae9f47048c6696f41b550c8e67a07
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-01 10:17:52 -0700

    Completed exercise

---
+++ javascript-loops/main.js
@@ -0,0 +1,73 @@
+/* exported getNumbersToTen,
+            getEvenNumbersToTwenty,
+            repeatWord,
+            logEachCharacter,
+            doubleAll,
+            getKeys,
+            getValues
+ */
+function getNumbersToTen() {
+  var numbers = [];
+  var current = 1;
+  while (current !== 11) {
+    numbers[current - 1] = current;
+    current++;
+  }
+}
+
+function getEvenNumbersToTwenty() {
+  var evenNumbers = [];
+  var currentNumber = 2;
+  var index = 0;
+  while (currentNumber !== 22) {
+    evenNumbers[index] = currentNumber;
+    index++;
+    currentNumber += 2;
+  }
+
+}
+
+function repeatWord(word, times) {
+  var count = 1;
+  var repeated = '';
+  for (count; count < times; count++) {
+    repeated += word;
+  }
+  return repeated;
+
+}
+function logEachCharacter(string) {
+  for (var i = 0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+}
+
+logEachCharacter('javascript');
+
+function doubleAll(numbers) {
+  var doubled = [];
+  for (var i = 0; i < numbers.length; i++) {
+    doubled.push(numbers[i] * 2);
+  }
+  return doubled;
+
+}
+
+function getKeys(object) {
+  var keys = [];
+  var key;
+  for (key in object) {
+    keys.push(key);
+  }
+  return keys;
+
+}
+function getValues(object) {
+  var values = [];
+  var key;
+  for (key in object) {
+    values.push(object[key]);
+  }
+  return values;
+
+}
