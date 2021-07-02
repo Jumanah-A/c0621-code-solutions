d0430602e60566a5b34b59e1134e75cf064a6ca0
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-02 16:30:18 -0700

    deleted unnecessary code

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,14 +1,17 @@
 /* exported compact */
 
+// set an array for the truthy values
+// go though every item
+// check if its falsy
+// if it isNaN, continue
+// else
+// push the value into the array
+// then return the array
 function compact(array) {
   var truthyArray = [];
   for (var i = 0; i < array.length; i++) {
     if (Boolean(array) === false || Boolean(array[i]) === false) {
-      if (Boolean(array) === false) {
-        return [];
-      } else {
-        continue;
-      }
+      continue;
     } else {
       truthyArray.push(array[i]);
     }
