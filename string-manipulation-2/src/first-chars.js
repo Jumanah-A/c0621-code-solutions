7f8b47fd0417e4be681dc7ec0f6d51acca3ab9f0
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-05 14:17:06 -0700

    Completed exercsie string manipulation 2

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,4 +1,6 @@
 /* exported firstChars */
-function firstChars() {
-
+// use the string method slice starting from index 0;
+// return the string rreturend by the method
+function firstChars(length, string) {
+  return string.slice(0, length);
 }
--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,13 @@
 /* exported lastChars */
+// if the length of tehparamneter is greater than that of the string
+// return the string itself
+// else
+// return the string returned by the slice method of the string by getting ths substring
+// of the last string using the length provided
+
+function lastChars(length, string) {
+  if (length > string.length) {
+    return string;
+  }
+  return string.slice(string.length - length);
+}
