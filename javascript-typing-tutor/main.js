4b149cecfad0b75b712723db006c0ca2e8e745d8
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-08 14:16:11 -0700

    Completed exercise and optional tasks (accuracy and play again)

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,27 +1,36 @@
 
 var $span = document.querySelectorAll('span');
 var index = 0;
-// var $incorrect = 0;
+var $incorrect = 0;
+var $hidden = document.querySelector('.hidden');
+var lettersTyped = 0;
 function handleKeyDown(event) {
+  lettersTyped++;
   if ($span[index].textContent === event.key && index < 29) {
     $span[index].className = 'letter correct';
     index++;
     $span[index].className = 'letter current';
   } else if ($span[index].textContent === event.key && index === 29) {
     $span[index].className = 'letter correct';
-    // console.log("in index === 29 block");
-    // playAgain(event);
-    // console.log("after calling playagain function");
+    $hidden.className = 'modal overlay-shadow';
+    index = 0;
+    for (var i = 0; i < $span.length; i++) {
+
+      $span[i].className = 'reset letter';
+    }
+    $span[index].className = 'letter current';
+    var accuracy = ((30 - $incorrect) / lettersTyped) * 100;
+    document.querySelector('.accuracy').textContent = 'Accuracy: ' + accuracy + '%';
   } else {
     $span[index].className = 'letter incorrect';
-    // $incorrect++;
+    $incorrect++;
   }
 }
 document.addEventListener('keydown', handleKeyDown);
-
-// var $hidden = document.querySelector('.hidden');
-// function playAgain(event)
-// {
-//   console.log("in play again function");
-//   $hidden.className = 'modal overlay-shadow';
-// }
+function handleButton(event) {
+  $hidden.className = 'hidden';
+}
+var $noButton = document.querySelector('.no-button');
+var $yesButton = document.querySelector('.yes-button');
+$noButton.addEventListener('click', handleButton);
+$yesButton.addEventListener('click', handleButton);
