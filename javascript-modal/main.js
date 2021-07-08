bb35d8c75e86ff2e734b676fb12b8d965bb43fff
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-07 17:53:06 -0700

    fix positioning

---
+++ javascript-modal/main.js
@@ -0,0 +1,20 @@
+
+// var $openModal = document.querySelector(".open-modal");
+// var $modal = document.querySelector(".modal");
+// var $noButton = document.querySelector(".no-button");
+// function handleOpenModal(event)
+// {
+//   $modal.showModal();
+// }
+// //add openmodal listener
+// remove the hidden elemnet from m the bdialogue page
+
+// $openModal.addEventListener('click',handleOpenModal);
+
+// function handleNoButton(event)
+// {
+//   console.log("close button ");
+//   $modal.close();
+// }
+// //add nobutton listener
+// $noButton.addEventListener('click', handleNoButton);
