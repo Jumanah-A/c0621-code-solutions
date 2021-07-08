86079854eb6031a19d7939adb6a3991f647255b1
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-08 10:11:41 -0700

    Completed javascript-typing-tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,13 +1,27 @@
 
 var $span = document.querySelectorAll('span');
 var index = 0;
-// console.log($span);
+// var $incorrect = 0;
 function handleKeyDown(event) {
-  if ($span[index].textContent === event.key) {
+  if ($span[index].textContent === event.key && index < 29) {
+    $span[index].className = 'letter correct';
     index++;
-
+    $span[index].className = 'letter current';
+  } else if ($span[index].textContent === event.key && index === 29) {
+    $span[index].className = 'letter correct';
+    // console.log("in index === 29 block");
+    // playAgain(event);
+    // console.log("after calling playagain function");
+  } else {
+    $span[index].className = 'letter incorrect';
+    // $incorrect++;
   }
-  // console.log($span[0].textContent);
-  // console.log(event.key);
 }
 document.addEventListener('keydown', handleKeyDown);
+
+// var $hidden = document.querySelector('.hidden');
+// function playAgain(event)
+// {
+//   console.log("in play again function");
+//   $hidden.className = 'modal overlay-shadow';
+// }
