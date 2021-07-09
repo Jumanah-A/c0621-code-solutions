5a13291dc631e29134478f0a76f2ec8728e4e770
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-08 20:18:00 -0700

    Finished one function must finish remianing two

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,14 @@
 /* exported titleCase */
+
+function titleCase(string) {
+  // var minor = ['as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to']
+  // var stringArray = string.split(" ");
+  // for(var i=0;i<stringArray.length;i++)
+  // {
+  //   if(stringArray[i])
+  //   {
+
+  //   }
+
+  // }
+}
