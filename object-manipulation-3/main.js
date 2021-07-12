7e1e43d7ffb4af07488ad8e0720bb6c7c1142954
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-12 09:46:39 -0700

    Started

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,6 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+// var playerOne = {name:"Jumanah",hand:[]};
+// var playerTwo = {name:"Jack",hand:[]};
+// var playerThree = {name:"Jane",hand:[]};
+// var playerFour = {name:"John", hand:[]};
