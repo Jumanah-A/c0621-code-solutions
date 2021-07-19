7a0f6ada23134fb911a9b080061635a4b4a94a4a
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-19 16:31:44 -0700

    Completed javascript-constructors

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,21 @@
+
+var anObject = new Object();
+var anArray = new Array();
+var aBoolean = new Boolean();
+var aFunction = new Function();
+var aString = new String();
+var aNumber = new Number();
+
+console.log('value of anObject:', anObject);
+console.log('value of anArray:', anArray);
+console.log('value of aBoolean:', aBoolean);
+console.log('value of aFunction:', aFunction);
+console.log('value of aString:', aString);
+console.log('value of aNumber:', aNumber);
+
+console.log('typeof of anObject:', typeof anObject);
+console.log('typeof of anArray:', typeof anArray);
+console.log('typeof of aBoolean:', typeof aBoolean);
+console.log('typeof of aFunction:', typeof aFunction);
+console.log('typeof of aString:', typeof aString);
+console.log('typeof of aNumber:', typeof aNumber);
