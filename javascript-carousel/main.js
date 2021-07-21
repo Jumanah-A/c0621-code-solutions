c43ff10e08a33fe65aa8b742c1b53f695007fac5
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-21 16:59:53 -0700

    edited interval resetting and set up gloabl index variable and eliminated indexOf() for efficiency

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,14 +1,21 @@
 var imageArray = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
 var $img = document.querySelector('img');
-
+var index = 0;
 function handleClick(event) {
   var $imgUrl = $img.getAttribute('src');
   if (event.target.className === 'fas fa-chevron-right fa-4x') {
+    clearInterval(intervalId);
+    intervalId = setInterval(myCallback, 3000);
     changeImage($imgUrl, 'right');
+
   } else if (event.target.className === 'fas fa-chevron-left fa-4x') {
+    clearInterval(intervalId);
+    intervalId = setInterval(myCallback, 3000);
     changeImage($imgUrl, 'left');
 
   } else if (event.target.className === 'far fa-circle') {
+    clearInterval(intervalId);
+    intervalId = setInterval(myCallback, 3000);
     document.querySelector('.current').className = 'far fa-circle';
     document.getElementById(event.target.id).className = 'current fas fa-circle';
     $img.setAttribute('src', imageArray[event.target.id]);
@@ -16,23 +23,22 @@ function handleClick(event) {
 }
 
 function changeImage(imgUrl, direction) {
-  var nextImageIndex = imageArray.indexOf(imgUrl);
   if (direction === 'right') {
-    if (imageArray.indexOf(imgUrl) === imageArray.length - 1) {
-      nextImageIndex = 0;
+    if (index === imageArray.length - 1) {
+      index = 0;
     } else {
-      nextImageIndex++;
+      index++;
     }
   } else if (direction === 'left') {
-    if (imageArray.indexOf(imgUrl) === 0) {
-      nextImageIndex = imageArray.length - 1;
+    if (index === 0) {
+      index = imageArray.length - 1;
     } else {
-      nextImageIndex--;
+      index--;
     }
   }
   document.querySelector('.current').className = 'far fa-circle';
-  document.getElementById(nextImageIndex).className = 'current fas fa-circle';
-  $img.setAttribute('src', imageArray[nextImageIndex]);
+  document.getElementById(index).className = 'current fas fa-circle';
+  $img.setAttribute('src', imageArray[index]);
 }
 
 var $carousel = document.querySelector('.carousel');
@@ -40,8 +46,8 @@ var $progressBar = document.querySelector('.progress-bar');
 $carousel.addEventListener('click', handleClick);
 $progressBar.addEventListener('click', handleClick);
 
-setInterval(myCallback, 3000);
 function myCallback() {
   var $imgUrl = $img.getAttribute('src');
   changeImage($imgUrl, 'right');
 }
+var intervalId = setInterval(myCallback, 3000);
