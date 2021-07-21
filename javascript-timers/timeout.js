c4eb99780e0c2730559015c92fe5aed339c067a0
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-20 17:25:53 -0700

    Completed exercise

---
+++ javascript-timers/timeout.js
@@ -0,0 +1,3 @@
+
+// var timeoutID = setTimeout(function(){ document.querySelector('h1').textContent = "Hello there";}, 2.0 * 1000);
+setTimeout(function () { document.querySelector('h1').textContent = 'Hello there'; }, 2.0 * 1000);
