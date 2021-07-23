e09de6771b4d2d60db57671ac54f53ac38ed4e0f
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-23 10:00:59 -0700

    working on it

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,25 +1,28 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 var players = [{ name: 'Jumanah', hand: [] },
-  { name: 'Jack', hand: [] },
-  { name: 'Jane', hand: [] },
-  { name: 'John', hand: [] }];
-// var playerOne = {name:"Jumanah",hand:[]};
-// var playerTwo = {name:"Jack",hand:[]};
-// var playerThree = {name:"Jane",hand:[]};
-// var playerFour = {name:"John", hand:[]};
+  { name: 'Jack', hand: [], score: 0 },
+  { name: 'Jane', hand: [], score: 0 },
+  { name: 'John', hand: [], score: 0 }];
 var suit = ['Club', 'Diamond', 'Heart', 'Spade'];
-var value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+var rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 var cardsArray = [];
 for (var i = 0; i < suit.length; i++) {
-  for (var j = 0; j < value.length; j++) {
-    cardsArray.push({ suit: suit[i], value: value[j] });
+  for (var j = 0; j < rank.length; j++) {
+    cardsArray.push({ suit: suit[i], rank: rank[j] });
   }
 }
 var shuffledDeck = _.shuffle(cardsArray);
 for (var k = 0; k < players.length; k++) {
-  // var x = shuffledDeck.splice(0, 2)
   players[k].hand = shuffledDeck.splice(0, 2);
+
+  // players[]
 }
 // console.log(shuffledDeck);
-// console.log(cardsArray);
-// console.log(players);
+console.log(cardsArray);
+console.log(players);
+
+// legened:
+// ace = 11
+// queen=10
+// jack=10
+// king
