0af402261ec1d0efae751f8e9a62399fe407b0c8
Jumanah Almajnouni <jumanahh@outlook.com>
2021-08-17 15:52:58 +0000

    Completed the exercise but can't get the put method to be called or to run

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 app.get('/api/notes',(req,res) =>
 {
   const notesArray = [];
-  console.log(Object.keys(data.notes).length);
   if (Object.keys(data.notes).length !== 0)
   {
     for(const key in data.notes)
@@ -26,10 +25,9 @@ app.get('/api/notes/:id', (req, res) => {
   {
     if ((Object.keys(data.notes[itemId]).includes('content')) && !(data.notes[itemId].content === "")) {
       res.status(200).send(data.notes[itemId]);
-
-    }else{
-      res.status(404).send({ "error": `Cannot find a note for id ${itemId }`});
     }
+  } else {
+    res.status(404).send({ "error": `Cannot find a note for id ${itemId}` });
   }
 })
 
@@ -65,14 +63,45 @@ app.delete('/api/notes/:id', (req,res) => {
   const itemId = req.params.id;
   if (itemId < 0) {
     res.status(400).send({ "error": "You have entered a negative ID number, please enter an appropriate ID number." });
-  } else if (Object.keys(data.notes).includes(itemId)) {
-    // if ((Object.keys(data.notes[itemId]).includes('content')) && !(data.notes[itemId].content === "")) {
-    //   res.status(200).send(data.notes[itemId]);
-
-    // } else {
-    //   res.status(404).send({ "error": `Cannot find a note for id ${itemId}` });
-    // }
+  } else if (!(Object.keys(data.notes[itemId]).includes('content')) || (data.notes[itemId].content === "")) {
+    res.status(404).send({ "error": `Cannot find a note for id ${itemId}` });
+  }else
+  {
+    delete data.notes[itemId];
+    fs.writeFile('data.json', JSON.stringify(data), err => {
+      if (err) {
+        res.status(500).send({ "error": "An unexpected error occurred." });
+      } else {
+        res.sendStatus(204);
+      }
+    });
   }
+});
+app.put('/api/notes:id',(req,res) => {
+  // res.send("oh no!")
+  // console.log("in the put method");
+  // console.log(req.body);
+  // const itemId = req.params.id;
+  // if(itemId < 0)
+  // {
+  //   res.status(400).send({ "error": "You have entered a negative ID number, please enter an appropriate ID number." });
+  // } else if (data.notes[itemId].content === "")
+  // {
+  //   es.status(404).send({ "error": `Cannot find a note for id ${itemId}` });
+  // } else if (!Object.keys(req.body).includes('content'))
+  // {
+  //   res.status(400).send({ "error": "content field is required" });
+  // }else
+  // {
+  //   data.notes[itemId] = req.body[content];
+  //   fs.writeFile('data.json', JSON.stringify(data), err => {
+  //     if (err) {
+  //       res.status(500).send({ "error": "An unexpected error occurred." });
+  //     } else {
+  //       res.sendStatus(200);
+  //     }
+  //   });
+  // };
 })
 
 
