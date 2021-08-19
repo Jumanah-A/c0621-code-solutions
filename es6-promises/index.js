35001e9b8d823bbe85716e30a1d02c85e86ccf0e
Jumanah Almajnouni <jumanahh@outlook.com>
2021-08-19 00:40:40 +0000

    Used the reason and value parameters to log the output

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -2,8 +2,7 @@ const takeAChance = require('./take-a-chance');
 const promiseObject = takeAChance('Jumanah');
 
 promiseObject.then(value => {
-  console.log("Hooray! You're so lucky!");
+  console.log(value);
 }, reason => {
-  var e = new Error("It's just bad luck!");
   console.log(reason.message);
 });
