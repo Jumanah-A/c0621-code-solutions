a7c4e39af47ff97eddd3fbff2b2087388a3e3e72
Jumanah Almajnouni <jumanahh@outlook.com>
2021-08-23 17:04:46 +0000

    started on the authorization code code

---
+++ testing-spotify/authorization-code/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const request = require('request');
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieparser =  require('cookie-parser');
+
+
+const clientId = 'ed1e27a30e304a93a7643beeb9c29696';
+const clientSecret ='a8ab73c18bce44a88f4f9bc4c352ca76';
+const redirectUri = '';
