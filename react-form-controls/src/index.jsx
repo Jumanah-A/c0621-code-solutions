21d34fb401c8bc0de4a490ab3112f7c7d6286a9e
Jumanah Almajnouni <jumanahh@outlook.com>
2021-08-26 19:49:06 +0000

    Completed react-form-controls

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    const element =
+      <form onSubmit={this.handleSubmit}>
+        <label>
+            Email:
+            <input type="text" value={this.state.email} onChange={this.handleChange}></input>
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>;
+
+    return element;
+  }
+
+  handleChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log('State:', this.state);
+    event.preventDefault();
+
+  }
+}
+
+ReactDOM.render(<NewsletterForm />, document.getElementById('root'));
