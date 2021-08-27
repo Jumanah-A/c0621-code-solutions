6e081a3a6d0d3861111e08aaa52fc15bbec05b51
Jumanah Almajnouni <jumanahh@outlook.com>
2021-08-27 16:00:19 +0000

    Complted optional part of validated-input

--- react-validated-input-component/src/index.jsx
+++ react-validated-input-component/src/index.jsx
@@ -15,11 +15,11 @@ class PasswordValidation extends React.Component {
     const element =
       <form onSubmit={this.handleSubmit}>
         <div>
-            Password:
+          Password:
         </div>
         <div>
           <label>
-              <input type="password" value={this.state.email} onChange={this.handleChange}></input>
+            <input type="password" value={this.state.email} onChange={this.handleChange}></input>
           </label>
           {(this.state.password === '') &&
             <span>
@@ -29,10 +29,15 @@ class PasswordValidation extends React.Component {
           {((this.state.password.length < 8 && this.state.password !== '')) &&
             <span>
               <span className="icon"><ImCross className='cross' /></span>
-            <div><p className="error">Your password is too short.</p></div></span>
+              <div><p className="error">Your password is too short.</p></div></span>
           }
-          {(this.state.password.length >= 8) &&
-              <span className="icon"><ImCheckmark className='check' /></span>
+          {(this.validatePassword(this.state.password)) && (this.state.password.length >= 8) &&
+            <span className="icon"><ImCheckmark className='check' /></span>
+          }
+          {!(this.validatePassword(this.state.password)) && (this.state.password.length >= 8) &&
+            <span>
+              <span className="icon"><ImCross className='cross' /></span>
+              <div><p className="error">Make sure that your password contains a capital letter a number and a special character!</p></div></span>
           }
         </div>
 
@@ -47,6 +52,12 @@ class PasswordValidation extends React.Component {
   handleChange(event) {
     this.setState({ password: event.target.value });
   }
+
+  validatePassword(password) {
+    // const regex = new RegExp('(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])');
+    const regex = /(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])/;
+    return regex.test(password);
+  }
 }
 
-ReactDOM.render(<PasswordValidation/>, document.getElementById('root'));
+ReactDOM.render(<PasswordValidation />, document.getElementById('root'));
